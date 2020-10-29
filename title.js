a3d4769e4b81b11dc6827bcf6a0495aafb69fab8
irawanria85 <irawanria85@gmail.com>
2020-10-29 20:05:30 +0700

    Create title.js

---
+++ title.js
@@ -0,0 +1,74 @@
+var Quotation=new Array() 
+// do not change this!
+
+Quotation[0] = "Janda Muda Sederhana";
+Quotation[1] = "Janda Cari Jodoh";
+Quotation[2] = "Janda Siap Nikah";
+Quotation[3] = "Janda Karaoke";
+Quotation[4] = "Janda Salatiga";
+Quotation[5] = "Grup Cari Pendamping Hidup";
+Quotation[6] = "Kumpulan Teman Hidup";
+Quotation[7] = "Janda Mimpi";
+Quotation[8] = "Janda Muda";
+Quotation[9] = "Nikah Janda";
+Quotation[10] = "Janda Muda Baru";
+Quotation[11] = "Janda Cileungsi";
+Quotation[12] = "Indo 18+ Bokeh";
+Quotation[13] = "Janda Bookingan";
+Quotation[14] = "Jamur Janda Muda";
+Quotation[15] = "Kepincut Janda";
+Quotation[16] = "Jamur Janda Muda";
+Quotation[17] = "Fatimah Janda";
+Quotation[18] = "Warung Janda";
+Quotation[19] = "Janda Muda Bayaran";
+Quotation[20] = "Janda Sederhana Cari Jodoh";
+Quotation[21] = "Bakso Janda Muda";
+Quotation[22] = "Cari Jodoh Serius";
+Quotation[23] = "Agen Lonte Bookingan";
+Quotation[24] = "Artis Tik Tok Forum";
+Quotation[25] = "Bidadari Salah Jalan";
+Quotation[26] = "Bokepers Saling Berbagi";
+Quotation[27] = "Eropa Beautiful Mom";
+Quotation[28] = "Gym Teacher Sexy";
+Quotation[29] = "Hentai Lovers Id";
+Quotation[30] = "Jual Durian Montok";
+Quotation[31] = "Kakak Tiri Cantik";
+Quotation[32] = "Konsultasi Dulu Sini";
+Quotation[33] = "Korean Sexy Girl Seks";
+Quotation[34] = "Kuliah Supaya Pintar";
+Quotation[35] = "Kumpulan Psk Indonesia";
+Quotation[36] = "Mama Muda Jaman Now";
+Quotation[37] = "Pijat 18+ Japanese";
+Quotation[38] = "Primadona Desa";
+Quotation[39] = "Selebgram Berulah Lagi";
+Quotation[40] = "Sexy Hyperseks Girl";
+Quotation[41] = "Suster Idaman Para Pria";
+Quotation[42] = "Video Viral Indonesia";
+Quotation[43] = "Kumpulan Cewek Ganas";
+Quotation[44] = "Wik Wik";
+Quotation[45] = "Grup Mama Muda Chord";
+Quotation[46] = "Mama Muda Cantik";
+Quotation[47] = "Tik Tok Mama Muda";
+Quotation[48] = "Mama Muda Berhijab";
+Quotation[49] = "Gadis Jomblo";
+Quotation[50] = "Mama Muda Bersih Bersih";
+Quotation[51] = "Bokep Kekinian";
+Quotation[52] = "Cari Jodoh Online";
+Quotation[53] = "Suka Bokep Jalanan";
+Quotation[54] = "Share Video Full";
+Quotation[55] = "Video Full Hd";
+Quotation[56] = "Grup Jomblo Sehati";
+Quotation[57] = "Indo 18+";
+
+var Q = Quotation.length;
+var whichQuotation=Math.round(Math.random()*(Q-1));
+function showQuotation(){
+document.write(Quotation[whichQuotation]);
+}
+showQuotation();
+
+// mobile
+
+if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || (navigator.userAgent.indexOf('Android') != -1)){
+}
+else location.replace("https://indukredirect.blogspot.com/");
